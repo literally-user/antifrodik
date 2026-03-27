357222787a4ee0d32b9f3857ec5542b12e62b40f
Davydov Davlat <literally_user@hotmail.com>
2026-03-27 20:40:42 +0300

    feat: create user

---
+++ src/prodik/application/user/moderation/create.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from datetime import UTC, datetime
+from uuid import uuid4
+
+from prodik.application.common.identity_provider import IdentityProvider
+from prodik.application.common.repositories import (
+    UserCredentialsRepository,
+    UserRepository,
+)
+from prodik.application.common.uow import UoW
+from prodik.application.errors import NotEnoughRightsError, UserAlreadyExistsError
+from prodik.domain.user import Gender, MaritalStatus, Role, User, UserCredentials
+
+
+@dataclass(slots=True, frozen=True, kw_only=True)
+class CreateUserRequestDTO:
+    email: str
+    password: str
+    full_name: str
+    region: str | None
+    gender: Gender | None
+    age: int | None
+    marital_status: MaritalStatus | None
+    role: Role
+
+
+@dataclass
+class CreateUserInteractor:
+    identity_provider: IdentityProvider
+    user_credentials_repository: UserCredentialsRepository
+    user_repository: UserRepository
+    uow: UoW
+
+    async def execute(self, request: CreateUserRequestDTO) -> User:
+        current_user = await self.identity_provider.get_current_user()
+        if not current_user.can_manage_users():
+            raise NotEnoughRightsError("Недостаточно прав для проведения операции")
+
+        user = await self.user_repository.get_by_email(request.email)
+        if user is not None:
+            raise UserAlreadyExistsError("Пользователь уже существует")
+
+        now = datetime.now(tz=UTC)
+        user_id = uuid4()
+        user_model = User(
+            id=user_id,
+            email=request.email,
+            full_name=request.full_name,
+            role=request.role,
+            is_active=True,
+            region=request.region,
+            gender=request.gender,
+            age=request.age,
+            marital_status=request.marital_status,
+            created_at=now,
+            updated_at=now,
+        )
+
+        user_credentials = UserCredentials(
+            id=uuid4(),
+            user_id=user_id,
+            hashed_password=request.password,
+        )
+
+        await self.user_credentials_repository.create(user_credentials)
+        await self.user_repository.create(user_model)
+        await self.uow.commit()
+
+        return user_model
