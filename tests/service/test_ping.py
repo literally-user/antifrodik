8bb709488872ea3431632d8e1d4804663d4d9ceb
Davydov Davlat <literally_user@hotmail.com>
2026-03-29 12:53:57 +0300

    some changes

--- tests/service/test_ping.py
+++ tests/service/test_ping.py
@@ -1,8 +1,10 @@
 import pytest
 from httpx import AsyncClient
+from dirty_equals import IsPartialDict
 
 @pytest.mark.asyncio
-async def test_login(test_client: AsyncClient) -> None:
+async def test_ping(test_client: AsyncClient) -> None:
     response = await test_client.get('/ping')
 
     assert response.status_code == 200
+    assert response.json() == IsPartialDict(message="pong")
