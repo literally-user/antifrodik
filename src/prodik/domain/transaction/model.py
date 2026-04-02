2ccae661110f0b43dbdc0adc5e5720c8cbb173c8
Davydov Davlat <literally_user@hotmail.com>
2026-04-02 08:51:25 +0300

    feat: transaction model

---
+++ src/prodik/domain/transaction/model.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import StrEnum
+from typing import Any
+from uuid import UUID
+
+
+type TransactionMetadata = dict[str, Any]
+
+class TransactionStatus(StrEnum):
+    APPROVED = "APPROVED"
+    DECLINED = "DECLINED"
+
+class TransactionChannel(StrEnum):
+    WEB = 'WEB'
+    MOBILE = 'MOBILE'
+    POS = 'POS'
+    OTHER = 'OTHER'
+
+@dataclass(kw_only=True)
+class TransactionLocation:
+    country: str | None
+    city: str | None
+    latitude: float | None
+    longitude: float | None
+
+@dataclass(kw_only=True)
+class Transaction:
+    id: UUID
+    user_id: UUID
+    amount: float
+    currency_code: str
+    status: TransactionStatus
+    merchant_id: str | None
+    merchant_category_code: str | None
+    timestamp: str | None
+    ip_address: str | None
+    device_id: str | None
+    channel: TransactionChannel | None
+    location: TransactionLocation | None
+    metadata: TransactionMetadata | None
+    is_fraud: bool
+    created_at: datetime
