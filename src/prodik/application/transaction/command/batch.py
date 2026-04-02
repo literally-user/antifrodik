54d709aad0eddda1e02433ed4fc4c8d5c987e8cf
Davydov Davlat <literally_user@hotmail.com>
2026-04-02 11:39:23 +0300

    chore: some lint changes

--- src/prodik/application/transaction/command/batch.py
+++ src/prodik/application/transaction/command/batch.py
@@ -136,7 +136,9 @@ class BatchTransactionsInteractor:
                 rule_results.append(rule_result)
 
             transaction_model.is_fraud = any(
-                result.matched for result in rule_results if result.transaction_id == transaction_model.id
+                result.matched
+                for result in rule_results
+                if result.transaction_id == transaction_model.id
             )
             if transaction_model.is_fraud:
                 transaction_model.status = TransactionStatus.DECLINED
